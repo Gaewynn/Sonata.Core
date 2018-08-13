e818d3a9a635f8c7950133ec9097dde96f119584
gaewynn <af67@outlook.com>
2018-08-13 10:24:01 +0200

    Adding ServiceResponse as a Core component

---
+++ Sonata.Core/CoreProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace Sonata.Core
+{
+	public class CoreProvider
+    {
+		public static void Trace(Assembly source, string message)
+		{
+			Trace(source == null ? String.Empty : source.FullName, message);
+		}
+
+		public static void Trace(string source, string message)
+		{
+
+		}
+	}
+}
---
+++ Sonata.Core/Logging/Log.cs
@@ -0,0 +1,127 @@
+﻿#region Namespace Sonata.Core.Logging
+//	TODO
+#endregion
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Sonata.Core.Logging
+{
+	[DataContract(Name = "log")]
+	public class Log
+	{
+		#region Properties
+
+		[DataMember(Name = "level")]
+		public LogLevel Level { get; set; }
+
+		[DataMember(Name = "code")]
+		public string Code { get; set; }
+
+		[DataMember(Name = "message")]
+		public string Message { get; set; }
+
+		#endregion
+
+		#region Methods
+
+		#region Object Members
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			if (!String.IsNullOrWhiteSpace(Code))
+			{
+				sb.Append(Code);
+				if (!String.IsNullOrWhiteSpace(Message))
+					sb.Append(" - ");
+			}
+
+			if (!String.IsNullOrWhiteSpace(Message))
+				sb.Append(Message);
+
+			return sb.ToString();
+		}
+
+		#endregion
+
+		public static Log BuildTrace(string message)
+		{
+			return BuildTrace(null, message);
+		}
+
+		public static Log BuildTrace(string code, string message)
+		{
+			return Build(LogLevel.Trace, code, message);
+		}
+
+		public static Log BuildDebug(string message)
+		{
+			return BuildDebug(null, message);
+		}
+
+		public static Log BuildDebug(string code, string message)
+		{
+			return Build(LogLevel.Debug, code, message);
+		}
+
+		public static Log BuildInformation(string message)
+		{
+			return BuildInformation(null, message);
+		}
+
+		public static Log BuildInformation(string code, string message)
+		{
+			return Build(LogLevel.Information, code, message);
+		}
+
+		public static Log BuildWarning(string message)
+		{
+			return BuildWarning(null, message);
+		}
+
+		public static Log BuildWarning(string code, string message)
+		{
+			return Build(LogLevel.Warning, code, message);
+		}
+
+		public static Log BuildError(string message)
+		{
+			return BuildError(null, message);
+		}
+
+		public static Log BuildError(string code, string message)
+		{
+			return Build(LogLevel.Error, code, message);
+		}
+
+		public static Log BuildCritical(string message)
+		{
+			return BuildCritical(null, message);
+		}
+
+		public static Log BuildCritical(string code, string message)
+		{
+			return Build(LogLevel.Critical, code, message);
+		}
+
+		public static Log Build(LogLevel level, string message)
+		{
+			return Build(level, null, message);
+		}
+
+		public static Log Build(LogLevel level, string code, string message)
+		{
+			return new Log
+			{
+				Level = level,
+				Code = code,
+				Message = message
+			};
+		}
+
+		#endregion
+	}
+}
